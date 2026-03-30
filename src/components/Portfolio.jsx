21ae133603369536bbdd570c9fd91cc59b67f59f
Gaurav Salunke <salunkegaurav2916@gmail.com>
2026-03-31 00:38:21 +0530

    added IPL live link and GitHub link

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -160,11 +160,28 @@ export default function Portfolio() {
           <div className="p-6 bg-[#0f172a] rounded-xl">
             <p className="text-yellow-400 text-xs mb-2">● IN DEVELOPMENT</p>
             <h3 className="text-xl">IPL Auction System</h3>
-            <p className="text-gray-400">Real-time bidding system.</p>
+            <p className="text-gray-400">
+  Real-time IPL auction system with dynamic bidding and team purse management.
+</p>
             <div className="flex gap-4 mt-4">
-              <span className="px-4 py-2 border border-gray-600 rounded">GitHub</span>
-              <span className="px-4 py-2 bg-gray-700 rounded">Coming Soon</span>
-            </div>
+
+  <a 
+    href="https://github.com/Gaurav-29-eng/ipl-auction"
+    target="_blank"
+    className="px-4 py-2 border border-green-500 rounded hover:bg-green-500 hover:text-black transition"
+  >
+    GitHub
+  </a>
+
+  <a 
+    href="https://ipl-auction-hazel.vercel.app/"
+    target="_blank"
+    className="px-4 py-2 bg-green-500 text-black rounded hover:scale-105 transition"
+  >
+    Live
+  </a>
+
+</div>
           </div>
 
           {/* PORTFOLIO */}
